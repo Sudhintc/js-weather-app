04ccd734d0842e1eb962efcf192d37eb5c7efcd4
sudhin tc <tcsudhin@gmail.com>
2023-05-11 17:12:39 +0530

    updated-suggestions

--- index.test.js
+++ index.test.js
@@ -1,71 +1,90 @@
-const { getWeather, displayWeatherData } = require("./index");
+const { getWeather, displayWeatherData, showWeather } = require("./index");
+// import { getWeather, displayWeatherData, showWeather } from './index.js';
 
-describe("getWeather", () => {
+describe("Weather app:", () => {
   beforeEach(() => {
     jest.resetAllMocks();
   });
-  test("should fetch weather data from API and display it", async () => {
-    const mockedData = {
-      name: "Chennai",
-      main: { temp: 20, humidity: 50 },
-      wind: { speed: 10 },
-      weather: [{ main: "Clouds" }],
-    };
 
-    global.fetch = jest.fn().mockResolvedValue(mockedData);
+  describe("getWeather()", () => {
+    describe("should fetch weather data from API", () => {
+      describe("when there is a successful API response", () => {
+        const mockedData = {
+          name: "Chennai",
+          main: { temp: 20, humidity: 50 },
+          wind: { speed: 10 },
+          weather: [{ main: "Clouds" }],
+        };
 
-    document.body.innerHTML = `
-      <input id="city" value="Chennai">
-      <div class="temp"></div>
-      <div class="city"></div>
-      <div class="humidity"></div>
-      <div class="wind"></div>
-      <img id="weather-img">
-      <div class="error"></div>
-      <div class="weather"></div>
-    `;
+        beforeEach(() => {
+          global.fetch = jest.fn().mockResolvedValue(mockedData);
+        });
 
-    await getWeather();
-    displayWeatherData(mockedData);
+        test("it should display the weather", async () => {
+          document.body.innerHTML = `
+            <input id="city" value="Chennai">
+            <div class="temp"></div>
+            <div class="city"></div>
+            <div class="humidity"></div>
+            <div class="wind"></div>
+            <img id="weather-img">
+            <div class="error"></div>
+            <div class="weather"></div>
+          `;
 
-    expect(fetch).toHaveBeenCalledWith(
-      "https://api.openweathermap.org/data/2.5/weather?q=Chennai&appid=224683afb327839fa35d0e7df7029bfd&units=metric"
-    );
+          await showWeather();
+          displayWeatherData(mockedData);
 
-    expect(document.querySelector(".temp").innerHTML).toBe("20°C");
-    expect(document.querySelector(".city").innerHTML).toBe("Chennai");
-    expect(document.querySelector(".humidity").innerHTML).toBe("50%");
-    expect(document.querySelector(".wind").innerHTML).toBe("10 Km/h");
-    expect(document.querySelector("#weather-img").src).toContain("clouds.png");
-    expect(document.querySelector(".error").style.display).toBe("none");
-    expect(document.querySelector(".weather").style.display).toBe("block");
-  });
-});
+          expect(fetch).toHaveBeenCalledWith(
+            "https://api.openweathermap.org/data/2.5/weather?q=Chennai&appid=224683afb327839fa35d0e7df7029bfd&units=metric"
+          );
 
-describe("api error", () => {
-  beforeEach(() => {
-    jest.resetAllMocks();
-  });
-  test("Handle invalid city", async () => {
-    const invalidCity = "not valid";
-    global.fetch = jest
-      .fn()
-      .mockRejectedValue(new Error("weather data not found"));
-    document.body.innerHTML = `
-  <input id = "city" value="${invalidCity}">
-  <div class='error'></div>
-  <div class="weather"></div>
-  `;
+          expect(document.querySelector(".temp").innerHTML).toBe("20°C");
+          expect(document.querySelector(".city").innerHTML).toBe("Chennai");
+          expect(document.querySelector(".humidity").innerHTML).toBe("50%");
+          expect(document.querySelector(".wind").innerHTML).toBe("10 Km/h");
+          expect(document.querySelector("#weather-img").src).toContain(
+            "clouds.png"
+          );
+          expect(document.querySelector(".error").style.display).toBe("none");
+          expect(document.querySelector(".weather").style.display).toBe(
+            "block"
+          );
+        });
+      });
 
-    await getWeather();
+      describe("when there is an unsuccessful API response", () => {
+        const error = new Error("weather data not availiable");
 
-    expect(fetch).toHaveBeenCalledWith(
-      `https://api.openweathermap.org/data/2.5/weather?q=${invalidCity}&appid=224683afb327839fa35d0e7df7029bfd&units=metric`
-    );
+        beforeEach(() => {
+          global.fetch = jest.fn().mockRejectedValue(error);
+        });
 
-    expect(document.querySelector(".error").innerHTML).toBe(
-      "weather data not availiable"
-    );
-    expect(document.querySelector(".weather").style.display).toBe("none");
+        test("it should display the error", async () => {
+          const invalidCity = "not valid";
+          document.body.innerHTML = `
+            <input id="city" value="${invalidCity}">
+            <div class="temp"></div>
+            <div class="city"></div>
+            <div class="humidity"></div>
+            <div class="wind"></div>
+            <img id="weather-img">
+            <div class="error"></div>
+            <div class="weather"></div>
+          `;
+
+          await showWeather();
+
+          expect(fetch).toHaveBeenCalledWith(
+            `https://api.openweathermap.org/data/2.5/weather?q=${invalidCity}&appid=224683afb327839fa35d0e7df7029bfd&units=metric`
+          );
+
+          expect(document.querySelector(".error").innerHTML).toBe(
+            "weather data not availiable"
+          );
+          expect(document.querySelector(".weather").style.display).toBe("none");
+        });
+      });
+    });
   });
 });
